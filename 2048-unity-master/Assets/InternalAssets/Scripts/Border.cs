d54c6a439a1e09f4f44c84a6a8e32b88a0aa26df
Rimuru <Rimuru.Dev@gmail.com>
2022-12-01 18:44:37 +0400

    Create Border.cs

---
+++ 2048-unity-master/Assets/InternalAssets/Scripts/Border.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct Border
+{
+    [Range(0, 100), SerializeField] public float Offset;    // = 0.05f;
+    [Range(0, 100), SerializeField] public float Spacing;   // = 0.1f;
+}
